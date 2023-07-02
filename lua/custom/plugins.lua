bb969b2e7e8afe24c30450c64d0d9e3e8bda417c
fberti <norbert.fehervari@gmail.com>
2023-07-03 00:21:20 +0200

    added folke/flash.nvim plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -14,14 +14,14 @@ local plugins = {
       require "custom.configs.lspconfig"
     end,
   },
-	{
-		"rust-lang/rust.vim",
-		ft = "rust",
-		init = function()
-			vim.g.rustfmt_autosave = 1
-		end
-	},
-	{
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
     "saecki/crates.nvim",
     ft = {"rust","toml"},
     config = function(_, opts)
@@ -38,5 +38,53 @@ local plugins = {
       return M
     end,
   },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Flash Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
+  }
 }
 return plugins
