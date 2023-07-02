35793fb9ef0764ee87b3c17ea0c785f168ff68da
Norbert Fehérvári <norbert.fehervari@gmail.com>
2023-07-02 02:51:41 +0200

    added cargo.toml plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -21,5 +21,22 @@ local plugins = {
 			vim.g.rustfmt_autosave = 1
 		end
 	},
+	{
+    "saecki/crates.nvim",
+    ft = {"rust","toml"},
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require("plugins.configs.cmp")
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  },
 }
 return plugins
